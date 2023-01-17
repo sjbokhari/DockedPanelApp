cbf8c7777d2a571a267d91574ce66dc08275ba89
sjbokhari <shehryar@bokhari.de>
2023-01-17 22:59:58 +0100

    added model for entityframe work

---
+++ DockedPanelApp/Database/Crud.cs
@@ -0,0 +1,44 @@
+﻿using DockedPanelApp.Database.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DockedPanelApp.Database
+{
+    public class Crud
+    {
+        //TODO: Connect to database https://learn.microsoft.com/de-de/ef/core/get-started/overview/first-app?tabs=visual-studio
+
+        //using var db = new BloggingContext();
+
+        //// Note: This sample requires the database to be created before running.
+        //Console.WriteLine($"Database path: {db.DbPath}.");
+
+        //// Create
+        //Console.WriteLine("Inserting a new blog");
+        //db.Add(new Blog { Url = "http://blogs.msdn.com/adonet" });
+        //db.SaveChanges();
+
+        //// Read
+        //Console.WriteLine("Querying for a blog");
+        //var blog = db.Blogs
+        //    .OrderBy(b => b.BlogId)
+        //    .First();
+
+        //// Update
+        //Console.WriteLine("Updating the blog and adding a post");
+        //blog.Url = "https://devblogs.microsoft.com/dotnet";
+        //blog.Posts.Add(
+        //    new Post { Title = "Hello World", Content = "I wrote an app using EF Core!" });
+        //db.SaveChanges();
+
+        //// Delete
+        //Console.WriteLine("Delete the blog");
+        //db.Remove(blog);
+        //db.SaveChanges();
+
+
+    }
+}
